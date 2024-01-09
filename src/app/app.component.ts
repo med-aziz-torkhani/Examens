c41ef68bce608888d0715dd18181596644affe0c
unknown <https://github.com/med-aziz-torkhani/Examens.git>
2024-01-10 00:36:26 +0100

    Premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { faCheck} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+
 })
 export class AppComponent {
   title = 'aziz-torkhani-app';
+  faC=faCheck
 }
